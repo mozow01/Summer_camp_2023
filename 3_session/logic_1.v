5c7c1df21af8c578d277bc4b87e28360c03e030d
mozow01 <78300414+mozow01@users.noreply.github.com>
2023-08-02 13:38:47 +0200

    Add files via upload

--- 3_session/logic_1.v
+++ 3_session/logic_1.v
@@ -1,2 +1,22 @@
 Theorem Chain_rule : forall A B C : Prop, ((A -> B) /\ (B -> C)) -> A -> C.
 Admitted.
+
+Search bool.
+
+Print orb.
+
+Parameter x : bool.
+
+Eval compute in (orb true x).
+
+Theorem classical_impl : forall x y : bool,
+orb (negb x) y = implb x y.
+Proof.
+intros x y.
+induction x.
+induction y.
+all: simpl; reflexivity. 
+Defined.
+
+
+
