103fa97990d7d3dd28d814057fa8b7a60ced0846
mozow01 <78300414+mozow01@users.noreply.github.com>
2023-08-02 12:14:43 +0200

    Add files via upload

--- 3_session/logic_1.lean
+++ 3_session/logic_1.lean
@@ -1,4 +1,23 @@
- -- Chain rule for →  
+-- Chain rule for →  
+
+-- (A → B) ∧ (B → C)
+-- ------------------
+--       A → C
+
+-- The idea of the proof:
+
+-- A ⊢  (A → B) ∧ (B → C)            
+-- -----------------                   
+--    A ⊢   A → B           A ⊢ A        A ⊢  (A → B) ∧ (B → C)
+--  -------------------------------       -----------------
+--                 A  ⊢ B                      A ⊢   B → C
+-- --------------------------------------------------------
+--                              A ⊢ C
+-- -------------------------------------------
+--                             A → C
+
+
+
 
 theorem Chain_rule : ∀ (A B C : Prop), ( (A → B) ∧ (B → C) ) → A → C :=
 begin
@@ -7,10 +26,7 @@ have H1 : A → B :=
 begin
 from and.left x,
 end,
-have H2 : B → C := 
-begin
-from and.right x,
-end,
+have H2 : B → C := and.right x,
 intros y,
 apply H2,
 apply H1,
