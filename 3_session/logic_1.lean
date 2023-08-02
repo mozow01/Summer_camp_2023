baa616181306b8a9a00d82e26f71e2f94a795510
mozow01 <78300414+mozow01@users.noreply.github.com>
2023-08-02 11:58:17 +0200

    Add files via upload

---
+++ 3_session/logic_1.lean
@@ -0,0 +1,22 @@
+
+
+
+
+
+theorem Chain_rule : 
+∀ (A B C : Prop), ( (A → B) ∧ (B → C) ) → A → C :=
+begin
+intros A B C x,
+have H1 : A → B :=
+begin
+from and.left x,
+end,
+have H2 : B → C := 
+begin
+from and.right x,
+end,
+intros y,
+apply H2,
+apply H1,
+exact 
+end
